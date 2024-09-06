b25b6b211d77ac6def460ba128bc04a0c516f631
solowihmaria <solowihmaria@gmail.com>
2024-09-06 13:30:40 +0300

    implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,34 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      // инициализация переменных
+      this.min = null;
+      this.max = null;
+      this.currentGuess = null; 
+    }
 
+    // начальный диапазон из теста берем
     setRange(min, max) {
-
+      this.min = min; 
+      this.max = max; 
     }
-
+    
+    // метод угадывания
     guess() {
-
-    }
-
+      // берём среднее значение между min и max, округляем до ближайшего целого числа
+      this.currentGuess = Math.round((this.min + this.max) / 2);
+      return this.currentGuess; 
+    } 
+    //  если предположение больше загаданного числа
     lower() {
-
+      // предположение становится максимальной границей 
+      this.max = this.currentGuess;
     }
-
+    //  если предположение меньше загаданного числа
     greater() {
-
+      // предположение становится минимальной границей
+      this.min = this.currentGuess;
     }
-}
-
+  }
+  
+  
 module.exports = GuessingGame;
